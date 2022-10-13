d2e151041b919335b4272e87a4af6ed390519726
Elektrokat <eddynathan4lyf@gmail.com>
2022-10-13 05:35:07 -0700

    ran a C file through the preprocessor

---
+++ 0x00-hello_world/hello.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - This is the entry of the code
+ *
+ * Return: Always returns 0 whwn successful
+ */
+int main(void)
+{
+	printf("Hello World!\n");
+	return (0);
+}
