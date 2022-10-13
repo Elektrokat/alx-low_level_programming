d8a776bfd1c9fb47080ce601c3a5b7863e4ef175
Elektrokat <eddynathan4lyf@gmail.com>
2022-10-13 06:45:00 -0700

    printed a line with puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - Code entry point
+ *
+ *Return: value is 0 for successful
+ */
+int main(void)
+{
+	puts("Programming is like building a multilingual puzzle\n");
+	return (0);
+}
