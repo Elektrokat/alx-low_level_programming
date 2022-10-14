92672777b2c253219e69e58047694061f9496d65
Elektrokat <eddynathan4lyf@gmail.com>
2022-10-14 10:01:30 -0700

    updated

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -10,14 +10,14 @@ int main(void)
 	char x;
 
 	for (x = 'a'; x <= 'z'; x++)
-
-		putchar(x);
-
+	{
+	putchar(x);
+	}
 	for (x = 'A'; x <= 'Z'; x++)
-
-		putchar(x);
-
-		putchar('\n');
+	{
+	putchar(x);
+	}
+	putchar('\n');
 
 	return (0);
 }
