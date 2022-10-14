8229e17eee403e2ffae1af7868fc8b79818b4e62
Elektrokat <eddynathan4lyf@gmail.com>
2022-10-14 13:50:11 -0700

    printed multiples of three

---
+++ 0x01-variables_if_else_while/101-print-comb4.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Return: always 0
+ */
+int main(void)
+{
+	int x, y, z;
+
+	for (x = 0; x < 10; x++)
+	{
+		for (y = x + 1; y < 10; y++)
+		{
+			for (z = y + 1; z < 10; z++)
+			{
+			putchar('0' + x);
+			putchar('0' + y);
+			putchar('0' + z);
+				if (x < 7 || y < 8 || z < 9)
+				{
+				putchar(',');
+				putchar(' ');
+				}
+			}
+		}
+	}
+
+	putchar('\n');
+
+	return (0);
+}
