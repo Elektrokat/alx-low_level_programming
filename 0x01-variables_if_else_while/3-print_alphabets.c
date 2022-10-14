87e0abdb0ea887c523077aa8a21b4626334a5311
Elektrokat <eddynathan4lyf@gmail.com>
2022-10-14 09:09:19 -0700

    printed lower and uppercase characters

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - entry point of code
+ *
+ * Return: always 0
+ */
+int main(void)
+{
+	char x;
+
+	for (x = 'a'; x <= 'z'; x++)
+
+		putchar(x);
+
+	for (x = 'A'; x <= 'Z'; x++)
+
+		putchar(x);
+
+		putchar('\n');
+
+	return (0);
+}
