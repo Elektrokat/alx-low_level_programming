214acc9b13608fb8e7b283ec10e41fa8679fa7d5
Elektrokat <eddynathan4lyf@gmail.com>
2022-10-14 10:40:14 -0700

    printing and integer with putchar

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Return: always 0
+ */
+int main(void)
+{
+	for (int x = 0; x < 10; x++)
+	{
+	putchar('0' + x);
+	}
+	putchar('\n');
+
+	return (0);
+}
