36bf1fe06068360a5ef0cc522c8acea059d0c873
Elektrokat <eddynathan4lyf@gmail.com>
2022-10-14 08:41:20 -0700

    printing the alphabets

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - entry poiht of code
+ *
+ * Return: always 0 if successful
+ */
+int main(void)
+{
+	char y;
+
+	for (y = 'a'; y <= 'z'; y++)
+
+		putchar(y);
+		putchar('\n');
+
+	return (0);
+}
