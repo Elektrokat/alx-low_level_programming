528869ea7fdf2bed3d00d6e6dcb4d9a331f8f1a5
Elektrokat <eddynathan4lyf@gmail.com>
2022-10-20 05:16:29 -0700

    testing debugging

--- 0x03-debugging/0-positive_or_negative.c
+++ 0x03-debugging/0-positive_or_negative.c
@@ -4,12 +4,17 @@
 #include "main.h"
 
 /**
-  * main - entry point
+  * positive_or_negative - prints the value of random numbers
+  *
+  *@i: a parameter
   *
   * Return: always 0
   */
 void positive_or_negative(int i)
 {
+	srand(time(0));
+	i = rand() - RAND_MAX / 2;
+
 	if (i == 0)
 		printf("%d is zero\n", i);
 	else if (i > 0)
