15d204f126580035675339b3953f911707548a02
Elektrokat <eddynathan4lyf@gmail.com>
2022-10-20 09:15:10 -0700

    hot fix

--- 0x03-debugging/2-main.c
+++ 0x03-debugging/2-main.c
@@ -3,21 +3,22 @@
 
 /**
 * main - prints the largest of 3 integers
+*
 * Return: 0
 */
 
 int main(void)
 {
 	int a, b, c;
-	int largest;
+	int result;
 
 	a = 972;
 	b = -98;
 	c = 0;
 
-	largest = largest_number(a, b, c);
+	result = largest_number(a, b, c);
 
-	printf("%d is the largest number\n", largest);
+	printf("%d is the largest number\n", result);
 
 	return (0);
 }
