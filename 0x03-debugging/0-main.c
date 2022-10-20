9d1b2659fab0ca502755be9cead828060a07ab75
Elektrokat <eddynathan4lyf@gmail.com>
2022-10-20 08:26:29 -0700

    hot fix

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,16 +1,21 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
 #include "main.h"
 
 /**
-  * main - tests function that prints if integer is positive or negative
-  * Return: 0
+  * positive_or_negative - prints the value of random numbers
+  *
+  *@i: a parameter
+  *
+  * Return: always 0
   */
-
-int main(void)
+void positive_or_negative(int i)
 {
-	int i;
-
-	i = 98;
-	positive_or_negative(i);
-
-	return (0);
+	if (i == 0)
+		printf("%d is zero\n", i);
+	else if (i > 0)
+		printf("%d is positive\n", i);
+	else
+		printf("%d is negative\n", i);
 }
