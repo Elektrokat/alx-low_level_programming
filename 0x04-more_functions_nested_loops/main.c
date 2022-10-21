1329c108fd64a27e3b115d53ed1af281eceb5919
Elektrokat <eddynathan4lyf@gmail.com>
2022-10-21 10:56:36 -0700

    returning digits

--- 0x04-more_functions_nested_loops/main.c
+++ 0x04-more_functions_nested_loops/main.c
@@ -1,13 +1,20 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
-  * main - entry point
-  *
-  * Return: 0
-  */
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
 int main(void)
 {
-	mul(8, 9);
+	char c;
+
+	c = '0';
+	printf("%c: %d\n", c, _isdigit(c));
+
+	c = 'a';
+	printf("%c: %d\n", c, _isdigit(c));
 
 	return (0);
 }
