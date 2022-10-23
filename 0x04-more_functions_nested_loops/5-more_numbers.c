80905c43b83a0f3351cce9c870e4f3e8f2d1e308
Elektrokat <eddynathan4lyf@gmail.com>
2022-10-23 11:24:40 -0700

    moon

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -7,11 +7,12 @@
   */
 void more_numbers(void)
 {
-	int a, b;
+	int a;
+	int b;
 
-	for (a = 0; a <= 14 ; a++)
+	for (a = 0; a <= 10; a++)
 	{
-		for (b = 0; b <= 10; b++)
+		for (b = 0; b <= 14; b++)
 		{
 			_putchar(b);
 		}
