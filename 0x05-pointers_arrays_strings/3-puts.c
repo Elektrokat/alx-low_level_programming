cc4c3fe9cee0c1648e1ccb48ed9aa0e1f22eeeeb
Elektrokat <eddynathan4lyf@gmail.com>
2022-10-25 11:24:30 -0700

    printing strings

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _puts - Prints a string to stdout.
+ *
+ * @str: The string to be printed.
+ *
+ * Return: 0
+ */
+void _puts(char *str)
+{
+	for (; *str != '\0'; str++)
+	{
+		_putchar(*str);
+	}
+
+	_putchar('\n');
+}
