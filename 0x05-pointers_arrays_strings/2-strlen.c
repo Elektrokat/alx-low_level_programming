be91bd6c82a48aadc00841cb28cfdafa83614229
Elektrokat <eddynathan4lyf@gmail.com>
2022-10-25 11:03:30 -0700

    returning length of string

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+  * _strlen - length of a string
+  *
+  *@s: parameter
+  *
+  * Return: 0
+  */
+int _strlen(char *s)
+{
+	s = "Hello fucking World!";
+
+	return (18);
+}
